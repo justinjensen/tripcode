0f5598c9f3657d067fc7533d19f03b99b6987b11
Justin Jensen <sqwwwd@gmail.com>
2015-09-10 04:43:49 -0400

    more 4chan like

--- lib/tripcode.rb
+++ lib/tripcode.rb
@@ -1,13 +1,25 @@
 require "tripcode/version"
 
 module Tripcode
+  SALT_TABLE =  ('.' * 47).split('') + ['/'] + ('0'..'9').to_a
+  SALT_TABLE += ('A'..'G').to_a + ('A'..'Z').to_a + ('a'..'f').to_a
+  SALT_TABLE += ('a'..'z').to_a + ('.' * 133).split('')
+
+  def self.sjis(str)
+    str.encode("shift_jis", invalid: :replace, undef: :replace, replace: '')
+  end
+  
+  def self.escape(str)
+    str.gsub(/[&<">]/, '&'=>'&amp;','<'=>'&lt;','"'=>'&quot;','>'=>'&gt;')
+  end
+  
   def self.hash(password)
     return "" if password.nil? || password.empty?
-    password = password.strip
-    password = password.gsub('&', '&amp;')
-    salt = (password + 'H.')[1..2]
-    salt.gsub!(/[^\.-z]/, '.')
-    salt.tr!(':;<=>?@[\]^_`','ABCDEFGabcdef')
+    password = Tripcode.sjis(password)
+    password = Tripcode.escape(password)
+    
+    salt =  SALT_TABLE["#{password}H."[1].ord % 256]
+    salt += SALT_TABLE["#{password}H."[2].ord % 256]
     return password.crypt(salt)[-10..-1].strip
   end
 end
--- test/tripcode_test.rb
+++ test/tripcode_test.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 require 'test_helper'
 
 class TripcodeTest < Minitest::Test
@@ -14,20 +15,36 @@ class TripcodeTest < Minitest::Test
   end
 
   def test_symbols
-    assert_equal "KNs1o0VDv6", Tripcode.hash("!")
-    assert_equal "z0MWdctOjE", Tripcode.hash("@")
-    assert_equal "u2YjtUz8MU", Tripcode.hash("#")
-    assert_equal "yflOPYrGcY", Tripcode.hash("$")
-    assert_equal "1t98deumW.", Tripcode.hash("%")
-    assert_equal "gBeeWo4hQg", Tripcode.hash("^")
-    assert_equal "MhCJJ7GVT.", Tripcode.hash("&")
-    assert_equal "o8gKYE6H8A", Tripcode.hash("*")
-    assert_equal "SGn2Wwr9CY", Tripcode.hash("(")
-    assert_equal "E9k1wjKgHI", Tripcode.hash(")")
+    assert_equal 'KNs1o0VDv6', Tripcode.hash('!')
+    assert_equal 'z0MWdctOjE', Tripcode.hash('@')
+    assert_equal 'u2YjtUz8MU', Tripcode.hash('#')
+    assert_equal 'yflOPYrGcY', Tripcode.hash('$')
+    assert_equal '1t98deumW.', Tripcode.hash('%')
+    assert_equal 'gBeeWo4hQg', Tripcode.hash('^')
+    assert_equal 'MhCJJ7GVT.', Tripcode.hash('&')
+    assert_equal 'o8gKYE6H8A', Tripcode.hash('*')
+    assert_equal 'SGn2Wwr9CY', Tripcode.hash('(')
+    assert_equal 'E9k1wjKgHI', Tripcode.hash(')')
+    assert_equal 'IHLbs/YhoA', Tripcode.hash('+')
+    assert_equal 'YeQQgdCJE6', Tripcode.hash(',')
+    assert_equal 'tHbGiobWdM', Tripcode.hash('-')
+    assert_equal 'XONm83jaIU', Tripcode.hash('.')
+    assert_equal 'DGAVybFN0A', Tripcode.hash('/')
+    assert_equal '8/08awL.AE', Tripcode.hash('\'')
+    assert_equal 'gt1azVccY2', Tripcode.hash('"')
+    assert_equal "7h2f0/nQ3w", Tripcode.hash("[")
+    assert_equal "rjM99frkZs", Tripcode.hash("]")
+    assert_equal "odBt7a7lv6", Tripcode.hash("{")
+    assert_equal "ATNP9hXHcg", Tripcode.hash("}")
+    assert_equal ".BmRMKOub2", Tripcode.hash(":")
+    assert_equal "zglc7ct1Ls", Tripcode.hash(";")
+    assert_equal "cPUZU5OGFs", Tripcode.hash("?")
+    assert_equal "l.nT/qC3Ro", Tripcode.hash("`")
+    assert_equal "LpJR0bXBSc", Tripcode.hash("~")
   end
 
   def test_bulk_tripcodes
-    File.open("./test/tripcodes.txt", "r") do |f|
+    File.open('./test/tripcodes.txt', 'r') do |f|
       f.each_line do |line|
         input, output = line.strip.split('!')
         assert_equal output, Tripcode.hash(input)
