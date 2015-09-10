e27fa23e64671433697878d01b425e2b1b17c2d6
Justin Jensen <sqwwwd@gmail.com>
2015-09-10 04:55:29 -0400

    refactor

--- lib/tripcode.rb
+++ lib/tripcode.rb
@@ -1,9 +1,6 @@
 require "tripcode/version"
 
 module Tripcode
-  SALT_TABLE =  ('.' * 47).split('') + ['/'] + ('0'..'9').to_a
-  SALT_TABLE += ('A'..'G').to_a + ('A'..'Z').to_a + ('a'..'f').to_a
-  SALT_TABLE += ('a'..'z').to_a + ('.' * 133).split('')
 
   def self.sjis(str)
     str.encode("shift_jis", invalid: :replace, undef: :replace, replace: '')
@@ -12,14 +9,21 @@ module Tripcode
   def self.escape(str)
     str.gsub(/[&<">]/, '&'=>'&amp;','<'=>'&lt;','"'=>'&quot;','>'=>'&gt;')
   end
+
+  def self.salt(password)
+    salt_table =  ('.' * 47).split('') + ['/'] + ('0'..'9').to_a
+    salt_table += ('A'..'G').to_a + ('A'..'Z').to_a + ('a'..'f').to_a
+    salt_table += ('a'..'z').to_a + ('.' * 133).split('')
+    salt =  salt_table["#{password}H.."[1].ord % 256]
+    salt += salt_table["#{password}H.."[2].ord % 256]
+    return salt
+  end
   
   def self.hash(password)
     return "" if password.nil? || password.empty?
     password = Tripcode.sjis(password)
     password = Tripcode.escape(password)
-    
-    salt =  SALT_TABLE["#{password}H."[1].ord % 256]
-    salt += SALT_TABLE["#{password}H."[2].ord % 256]
+    salt = Tripcode.salt(password)
     return password.crypt(salt)[-10..-1].strip
   end
 end
